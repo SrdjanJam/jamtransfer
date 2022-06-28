cf6b8c2b939c612121031dd1f62589f02c74fdd0
aleksanikolajevicjam <106659421+aleksanikolajevicjam@users.noreply.github.com>
2022-06-28 12:17:36 +0200

    Booking Admin forma
    Dodati fajlovi:
    Booking folder
    index.php
    index.tpl
    final.php
    scripts.php
    bookingAdmJS.tpl
    bookingJS.tpl
    scriptsAdm.tpl

--- plugins/Buking/index.php
+++ plugins/Buking/index.php
@@ -2,16 +2,6 @@
 $smarty->assign('page', $md->getName());
 @session_start();
 if (!$_SESSION['UserAuthorized']) die('Bye, bye');
-<<<<<<< Updated upstream
-$test = "test";
-$smarty->assign('test', $test);
-
-require_once ROOT . '/db/v4_Places.class.php';
-$pl = new v4_Places();
-if (isset($_SESSION['AgentID']) && $_SESSION['AgentID'] > 0) $AgentID = $_SESSION['AgentID'];
-else $AgentID = 0;
-
-=======
 /* echo "<pre>";
 var_dump($_SESSION);
 echo "<hr>";
@@ -30,47 +20,18 @@ $pl = new v4_Places();
 
 if (isset($_SESSION['AgentID']) && $_SESSION['AgentID'] > 0) $AgentID = $_SESSION['AgentID'];
 else $AgentID = 0;
->>>>>>> Stashed changes
 if (isset($_SESSION['FromID']) && $_SESSION['FromID'] > 0) {
     $fromID = $_SESSION['FromID'];
     global $pl;
     $pl->getRow($fromID);
     $fromName = $pl->getPlaceNameEN();
 }
-<<<<<<< Updated upstream
-
-=======
->>>>>>> Stashed changes
 if (isset($_SESSION['ToID']) && $_SESSION['ToID'] > 0) {
     $toID = $_SESSION['ToID'];
     global $pl;
     $pl->getRow($toID);
     $toName = $pl->getPlaceNameEN();
 }
-<<<<<<< Updated upstream
-
-if (isset($_SESSION['PaxNo']) && $_SESSION['PaxNo'] > 0)
-    $PaxNo = $_SESSION['PaxNo'];
-else $PaxNo = 0;
-
-if (s('MpaxTel') == '') $PaxTel = '';
-else $PaxTel = s('MPaxTel');
-if (isset($_SESSION['transferDate'])) $transferDate = $_SESSION['transferDate'];
-$_SESSION['REFRESHED'] = false;
-
-require_once "scriptsAdm.php";
-require_once ROOT . '/db/v4_AuthUsers.class.php';
-$au = new v4_AuthUsers();
-
-
-if (s('MPaxFirstName')=='') $PaxFirstName = " ";
-else $PaxFirstname=s('MpaxFirstName');
-$weby_key=file_get_contents(ROOT . '/plugins/Buking/weby_key.inc', FILE_USE_INCLUDE_PATH);
-
-// logika prikaza forme
-
-
-=======
 if (isset($_SESSION['PaxNo']) && $_SESSION['PaxNo'] > 0) $PaxNo = $_SESSION['PaxNo'];
 else $PaxNo = 0;
 
@@ -228,4 +189,3 @@ foreach($car as $VehicleCapacity => $price) {
 if ($_POST['VehicleID' != 0]) {
     echo "yes";
 }
->>>>>>> Stashed changes
