7a430c09c1ef39aa88ff60a459acea721e516d6e
MarkoJam <107544110+MarkoJam@users.noreply.github.com>
2022-11-03 10:37:06 +0100

    Update ListTemplate.php

--- plugins/SpecialDates/ListTemplate.php
+++ plugins/SpecialDates/ListTemplate.php
@@ -1,37 +1,74 @@
+<style>
+	.w-edit{
+		width:180px;
+	}
+</style>
+
 <script type="text/x-handlebars-template" id="ItemListTemplate">
 
+	<div class="row row-edit">
+		<div class="col-md-12">
+		
+			<div class="col-md-3">
+				<?=SPECIALDATE;?>
+			</div>
+
+			<div class="col-md-3">
+				<?=STARTTIME;?>
+			</div>	
+
+			<div class="col-md-3">
+				<?=ENDTIME;?>
+			</div>
+
+			<div class="col-md-3">
+				<?=CORRECTIONPERCENT;?>
+			</div>
+
+		</div>	
+	</div>
+
 	{{#each Item}}
-		<div  onclick="oneItem({{ID}});">
 		
-			<div class="row {{color}} pad1em listTile" 
-			style="border-top:1px solid #ddd" 
-			id="t_{{ID}}">
-		
-					<div class="col-md-3">
-						<strong>{{SpecialDate}}</strong>
-					</div>
+		<div class="row {{color}} pad1em listTile listTitleEdit" 
+		style="border-top:1px solid #ddd"
+		id="t_{{ID}}">
+	
+			<input type="hidden" name="OwnerID" id="OwnerID" class="w50" value="<?=$_SESSION['OwnerID'] ?>">
 
-					<div class="col-md-3">
-					    {{StartTime}}
-					</div>
+			<div class="col-md-12">
 
+				<!-- Example: -->
+				<!-- <div class="row">
 					<div class="col-md-3">
-					    {{EndTime}}
+						<label for="SpecialDate"><?=SPECIALDATE;?></label>
 					</div>
-
-					<div class="col-md-3">
-					    {{CorrectionPercent}}%
+					<div class="col-md-9">
+						<input type="text" name="SpecialDate" id="SpecialDate" class="w50 datepicker" value="{{SpecialDate}}">
 					</div>
-					
-			</div>
-		</div>
-		<div id="ItemWrapper{{ID}}" class="editFrame" style="display:none">
-			<div id="inlineContent{{ID}}" class="row">
-				<div id="one_Item{{ID}}" >
-					<?= LOADING ?>
+				</div> -->
+				
+				<div class="col-md-3">
+					<input type="text" name="SpecialDate" id="SpecialDate" class="w50 datepicker w-edit" value="{{SpecialDate}}">
 				</div>
-			</div>
-		</div>
+			
+				<div class="col-md-3">
+					<input type="text" name="StartTime" id="StartTime" class="w50 timepicker w-edit" value="{{StartTime}}">
+				</div>
+			
+				<div class="col-md-3">
+					<input type="text" name="EndTime" id="EndTime" class="w50 timepicker w-edit" value="{{EndTime}}">
+				</div>
+
+				<div class="col-md-3">
+					<input type="text" name="CorrectionPercent" id="CorrectionPercent" class="w25 w-edit" value="{{CorrectionPercent}}">%
+				</div>
+				
+			</div> <!-- End of .col-md-12 -->
+					
+
+		</div> <!-- End of .row -->
+
 
 	{{/each}}
 
