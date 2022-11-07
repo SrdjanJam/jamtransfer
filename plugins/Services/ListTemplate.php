7e75a27e27410782ffa3036a04fc600d4826d389
MarkoJam <107544110+MarkoJam@users.noreply.github.com>
2022-11-07 14:17:15 +0100

    Update ListTemplate.php

--- plugins/Services/ListTemplate.php
+++ plugins/Services/ListTemplate.php
@@ -1,38 +1,78 @@
 <script type="text/x-handlebars-template" id="ItemListTemplate">
 
+	<!-- Header labels: -->
+	<div class="row row-edit">
+		<div class="col-md-12">
+			<div class="col-md-1">
+				<?=SERVICE_ID;?>
+			</div>
 
+			<div class="col-md-2">
+				<?=ROUTENAME;?>
+			</div>	
+
+			<div class="col-md-2">
+				<?=VEHICLETYPENAME;?>
+			</div>
+
+			<div class="col-md-2">
+				<?=SERVICEPRICE1;?>
+			</div>
+			
+			<!-- <div class="col-md-2">
+				<?=DISCOUNT;?>
+			</div> -->
+
+			<div class="col-md-5">
+				<?=SURCATEGORY;?>
+			</div>
+
+		</div>				
+	</div>
+
+	<!-- dynamically content: -->
 	{{#each Item}}
 	
-			<div  onclick="oneItem({{ServiceID}});">		
-				<div class="row {{color}} pad1em listTile" 
-				style="border-top:1px solid #ddd" 
-				id="t_{{ServiceID}}">
-						
-						<div class="col-sm-1">
-							{{ServiceID}}
-						</div>							
-						<div class="col-sm-4">
-							{{RouteName}}
-						</div>							
-						<div class="col-sm-3">
-							{{VehicleTypeName}}
-						</div>						
-						<div class="col-sm-2">
-							{{ServicePrice1}}
-						</div>	
-						<div class="col-md-2"> 
-							<small><i class="fa fa-arrow-circle-up"></i> {{surCategory SurCategory}}</small>
-						</div>						
-	
+		<div class="row {{color}} pad1em listTile listTitleEdit" 
+		style="border-top:1px solid #ddd" 
+		id="t_{{ServiceID}}">
+
+			<div class="col-md-12">
+				<!-- ServiceID -->
+				<div class="col-md-1">
+					{{ServiceID}}
 				</div>
-			</div>
-			<div id="ItemWrapper{{ServiceID}}" class="editFrame" style="display:none">
-				<div id="inlineContent{{ServiceID}}" class="row">
-					<div id="one_Item{{ServiceID}}" >
-						<?= LOADING ?>
-					</div>
+				
+				<!-- RouteName: -->
+				<div class="col-md-2">
+					{{RouteName}}
+				</div>	
+				
+				<!-- VehicleTypeName: -->
+				<div class="col-md-2">
+					{{VehicleTypeName}}
 				</div>
-			</div>
+				
+				<!-- ServicePrice1(Active Prace): -->				
+				<div class="col-md-2">
+					<input type="text" name="ServicePrice1" id="ServicePrice1" class="w100" value="{{ServicePrice1}}" style="width:120px;">
+				</div>
+
+				<!-- Discount: -->
+				<!-- <div class="col-md-2">
+					<input type="text" name="Discount" id="Discount" class="w100" value="{{Discount}}" style="width:120px;">
+				</div> -->
+				
+
+				<!-- Price rules: -->
+				<div class="col-md-5 surcategory" data-status="{{PriceRules2}}" data-id="{{ServiceID}}">
+					<span class="show_hide">{{SurCategoryRB PriceRules 'SurCategory' '4' 'services' ServiceID}}</span>
+				</div>
+
+			</div>		
+
+		</div>
+
 	
 	{{/each}}
 
