179c6d4fea7cae33c73eb056c086dd8cbaa2a376
MarkoJam <107544110+MarkoJam@users.noreply.github.com>
2022-11-07 08:16:10 +0100

    Update ListTemplate.php

--- plugins/SpecialTimes/ListTemplate.php
+++ plugins/SpecialTimes/ListTemplate.php
@@ -1,5 +1,33 @@
 <script type="text/x-handlebars-template" id="ItemListTemplate">
 
+	<div class="row row-edit">
+		
+		<div class="col-md-2">
+			<?=VEHICLETYPEID;?>
+		</div>
+
+		<div class="col-md-2">
+			<?=STARTSEASON;?> - <?=ENDSEASON;?>
+		</div>	
+
+		<div class="col-md-2">
+			<?=SPECIALDATE;?>
+		</div>
+
+		<div class="col-md-2">
+			<?=WEEKDAYS;?>
+		</div>
+
+		<div class="col-md-2">
+			<?=STARTTIME;?> - <?=ENDTIME;?>
+		</div>
+
+		<div class="col-md-2">
+			<?=CORRECTIONPERCENT;?>
+		</div>
+					
+	</div>
+
 	{{#each Item}}
 		<div  onclick="oneItem({{ID}});">
 		
@@ -7,32 +35,33 @@
 			style="border-top:1px solid #ddd" 
 			id="t_{{ID}}">
 		
-					<div class="col-md-1">
-						<strong>{{VehicleTypeID}}</strong>
-					</div>
+				<div class="col-md-2">
+					<strong>{{VehicleTypeID}}</strong>
+				</div>
 
-					<div class="col-md-3">
-						<strong>{{StartSeason}} - {{EndSeason}}</strong>
-					</div>
+				<div class="col-md-2">
+					<strong>{{StartSeason}} - {{EndSeason}}</strong>
+				</div>
 
-					<div class="col-md-2">
-						<strong>{{SpecialDate}}</strong>
-					</div>
+				<div class="col-md-2">
+					<strong>{{SpecialDate}}</strong>
+				</div>
 
-					<div class="col-md-2">
-						<strong>{{WeekDays}}</strong>
-					</div>
+				<div class="col-md-2">
+					<strong>{{WeekDays}}</strong>
+				</div>
 
-					<div class="col-md-2">
-					    {{StartTime}}-{{EndTime}}
-					</div>
+				<div class="col-md-2">
+					{{StartTime}}-{{EndTime}}
+				</div>
 
-					<div class="col-md-2">
-					    {{CorrectionPercent}}%
-					</div>
+				<div class="col-md-2">
+					{{CorrectionPercent}}%
+				</div>
 					
 			</div>
 		</div>
+
 		<div id="ItemWrapper{{ID}}" class="editFrame" style="display:none">
 			<div id="inlineContent{{ID}}" class="row">
 				<div id="one_Item{{ID}}" >
