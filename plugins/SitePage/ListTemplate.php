5a916701d6c3bddb0e6ba5ba341eda35ad3fb67d
MarkoJam <107544110+MarkoJam@users.noreply.github.com>
2022-11-15 08:32:49 +0100

    Update ListTemplate.php

--- plugins/SitePage/ListTemplate.php
+++ plugins/SitePage/ListTemplate.php
@@ -1,28 +1,44 @@
 
 <script type="text/x-handlebars-template" id="ItemListTemplate">
 
+	<!-- Labels: -->
+	<div class="row row-edit">
+			
+		<div class="col-md-12">
+
+			<div class="col-md-3">
+				<?=ID;?>
+			</div>
+
+			<div class="col-md-9">
+				<?=TITLE;?>
+			</div>	
+
+		</div>
+	</div>
+
+	<!-- Main Content: -->
 	{{#each Item}}
 		<div  onclick="oneItem({{ID}});">
 		
 			<div class="row {{color}} pad1em listTile" 
 			style="border-top:1px solid #ddd" 
 			id="t_{{ID}}">
+
+				<div class="col-md-12">
 		
 					<div class="col-sm-3">
 						<strong>{{ID}}</strong>
 					</div>
 
-					<div class="col-sm-2">
+					<div class="col-sm-9">
 						{{Title}}
 					</div>
-
-					<div class="col-sm-2">
-					</div>
-
-					<div class="col-sm-3">
-					</div>
+				
+				</div>
 			</div>
 		</div>
+
 		<div id="ItemWrapper{{ID}}" class="editFrame" style="display:none">
 			<div id="inlineContent{{ID}}" class="row">
 				<div id="one_Item{{ID}}" >
