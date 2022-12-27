ca85d408317598c0deb58d277f722db243423634
SrdjanJam <97429881+SrdjanJam@users.noreply.github.com>
2022-12-27 10:17:20 +0100

    .

--- api/changeVisitStatus.php
+++
@@ -1,9 +0,0 @@
-<?
-
-    require_once ROOT . '/db/db.class.php';
-	$db = new DataBaseMysql();
-	$result = $db->RunQuery("UPDATE `v4_Visits` SET `Status`=1 WHERE `VisitID`=".$_SESSION['visitid']."");
-	echo "OK";
-
-
-
