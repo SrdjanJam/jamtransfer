76f622d5c999d93ccc59e6e9ea26051602eab108
MarkoJam <107544110+MarkoJam@users.noreply.github.com>
2022-12-28 13:21:51 +0100

    Update conn_string.php

--- conn_string.php
+++ conn_string.php
@@ -1,24 +1,38 @@
-<? 
-if(!isset($_SESSION)) session_start();
+<?
+
+// ("LOCAL",false) is Server
+// ("DEVELOPMENT",true) is Server
+
+if(!isset($_SESSION)) session_start(); 
+
+if(isset($_SESSION['AuthUserID'])) $_SESSION['AuthUserID'];
+else $_SESSION['AuthUserID'] = "";
+
 //ovde ubaciti userid-ije za user-e koji ce koristiti glavnu bazu a ne testnu
 $privusers=array(874,3012,3011,3013);
-if (in_array($_SESSION['AuthUserID'],$privusers)) define("DEVELOPMENT",false);
-else define("DEVELOPMENT",true);
+
+if (in_array($_SESSION['AuthUserID'],$privusers) ) define("DEVELOPMENT",false); // This is localhost
+else define("DEVELOPMENT",true); 
+
 define("MONITOR", 0);
 define("ALLOW_REFRESH", 1);
 define("CMS_ONLY", true); 
+
 if ($_SERVER['HTTP_HOST']=='wis.jamtransfer.com') define("LOCAL",false);
 else  define("LOCAL",true);
-if (LOCAL) {
+
+if (LOCAL) { // This is local host
 	define("ROOT_HOME", "http://localhost/jamtransfer/");
-	define("ROOT", "c:\\wamp\\www\\jamtransfer");
+	define("ROOT", "c:\\xampp\\htdocs\\jamtransfer");
 }	
-else {
+else { // Server
 	define("ROOT", $_SERVER['DOCUMENT_ROOT']);
 	define("ROOT_HOME", 'https://'.$_SERVER['HTTP_HOST'].'/');
-}	
+}
+
 define("ROOTPATH", ROOT.'/cms');
 define("SITE_CODE", '1');
+
 if (LOCAL) {
 	define("DB_HOST", "localhost");
 	define("DB_USER", "root");
@@ -35,8 +49,10 @@ else {
 	else define("DB_NAME", "jamtrans_touradria");
 }	
 
-if (DEVELOPMENT) error_reporting(E_ALL);
-//if (DEVELOPMENT) error_reporting(1);
+if (DEVELOPMENT) error_reporting(E_ALL); // Turn on error
+// if (DEVELOPMENT) error_reporting(1); // turn off error
+
 else error_reporting(E_ALL);
+
 
 ?>
