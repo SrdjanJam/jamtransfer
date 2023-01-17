a642fb1df67a9daf6f2741ba1134423942f79961
MarkoJam <107544110+MarkoJam@users.noreply.github.com>
2023-01-17 11:15:04 +0100

    Update index.php

--- plugins/Schedule/index.php
+++ plugins/Schedule/index.php
@@ -36,9 +36,22 @@ $op = new v4_Places();
 $or = new v4_Routes();
 $ol = new v4_OrderLog();
 
+
+$ordersArray = array();
+$sdArray = array();
+$sddArray = array();
+
+
 $BsColumnWidth = 12 / $NoColumns;
 $smarty->assign("BsColumnWidth",$BsColumnWidth);
-
+// Da li je interno
+$au->getRow($_SESSION['UseDriverID']);
+if($au->ContractFile == 'inter'){
+	$inter = true;
+}else{
+	$inter = false;
+}
+$smarty->assign("inter",$inter);
 // promjena pickup time
 $whereL = " WHERE Description LIKE '%PickupTime%'";
 $olKeys = $ol->getKeysBy('ID', 'DESC', $whereL);
@@ -47,25 +60,40 @@ foreach ($olKeys as $olid) {
 	$olKeys2[]=$ol->getDetailsID();
 	}
 
+// Svi transferi za hvatanje other transfer
+$details=array();
+// za proveru return transfer-a
+$q2 = "SELECT DetailsID,OrderID FROM v4_OrderDetails ORDER BY DetailsID DESC" ;
+$r2 = $db->RunQuery($q2);
+while ($t2 = $r2->fetch_object()) {
+	$row_array=array();
+	$row_array['DetailsID']=$t2->DetailsID; 
+	$row_array['OrderID']=$t2->OrderID; 
+	
+	$details[]=$row_array;
+}
+
 // dobavi sve transfere za odabrani datum za trenutnog vlasnika timetable-a
 $q = "
 SELECT *
 	FROM v4_OrderDetails, v4_OrdersMaster, v4_AuthUsers 
 	WHERE 
-		v4_OrderDetails.DriverID = " . $_SESSION['UseDriverID'] ; 
+		v4_OrderDetails.DriverID = " . $_SESSION['UseDriverID']; 
 $q .= " AND PickupDate >= '" . $DateFrom . "'"; 
 $q .= " AND PickupDate <= '" . $DateTo . "'";
 $q .= " AND TransferStatus < '6'";
 $q .= " AND TransferStatus != '4'"; 
-if ($DriverStatus==2) $q .= "	AND DriverConfStatus > 2 ";		  
+if ($DriverStatus==2) $q .= "	AND DriverConfStatus > 2 ";
 if ($DriverStatus==1) $q .= "	AND DriverConfStatus < 3 ";	
 $q .= " AND AuthUserID=UserID ";
 $q .= " AND MorderID=OrderID ";
 $q .= " ORDER BY DetailsID ASC";
 $r = $db->RunQuery($q);
 $subDArray = array();
+
+// $t
 while ($t = $r->fetch_object()) {
-	require("oneTransfer.php");
+	require("oneTransfer.php"); // oneTransfer.php ===========================================================================
 }
 $subDArray = array_unique($subDArray); // ostavi samo jedinstvene subdrivere u nizu
 $sdd="";
@@ -79,8 +107,8 @@ $q = "SELECT * FROM v4_AuthUsers";
 $q .= "	WHERE DriverID = " . $_SESSION['UseDriverID']; 
 //$q .= " WHERE AuthUserID in (".$sdd.") ORDER BY AuthUserRealName ASC";
 $r = $db->RunQuery($q);
-$sdArray = array();
-$sddArray = array();
+
+
 while ($d = $r->fetch_object()) {
 	if (in_array($d->AuthUserID,$subDArray)) {
 		$row = array();
@@ -104,6 +132,32 @@ while ($d = $r->fetch_object()) {
 $smarty->assign('ordersArray',$ordersArray);
 $smarty->assign('sdArray',$sdArray);
 $smarty->assign('sddArray',$sddArray);
+
+
+
+function getOtherTransferIDArray ($DetailsID,$details) {
+	$key = array_search($DetailsID, array_column($details, 'DetailsID'));
+	$oid=$details[$key]['OrderID'];
+	$keys = array_keys(array_column($details, 'OrderID'),$oid);
+	$otherDetailsID=null;
+	if (count($keys) == 2) {
+		if ($DetailsID == $details[$keys[0]]['DetailsID']) {
+			$otherDetailsID=$details[$keys[1]]['DetailsID'];
+		}
+		else if ($DetailsID == $details[$keys[1]]['DetailsID']) {
+			$otherDetailsID=$details[$keys[0]]['DetailsID'];
+		}		
+	}	
+	return $otherDetailsID;
+}
+
+
+# Pretvaranje formata datuma
+function YMD_to_DMY ($date) {
+	$elementi = explode ('-', $date);
+	$new_date = $elementi[2] . '.' . $elementi[1] . '.' . $elementi[0];
+	return $new_date;
+}
 
 
 ?>
