305aa5772a4c6e6eb7bdeea286f419c5fc224a27
MarkoJam <107544110+MarkoJam@users.noreply.github.com>
2023-02-17 09:16:02 +0100

    Changed request folder

--- plugins/Request/All.php
+++ plugins/Request/All.php
@@ -1,19 +1,8 @@
 <?
 header('Content-Type: text/javascript; charset=UTF-8');
-error_reporting(E_PARSE);
+require_once 'Initial.php';
 
 @session_start();
-# init libs
-require_once '../../../../db/db.class.php';
-require_once '../../../../db/v4_Request.class.php';
-
-
-# init class
-$db = new v4_Request();
-
-#********************************************
-# ulazni parametri su where, status i search
-#********************************************
 
 # sastavi filter - posalji ga $_REQUEST-om
 # sastavi filter prema Active
@@ -32,6 +21,7 @@ else if (isset($_REQUEST['active']) and $_REQUEST['active'] == 3) {
 else if (isset($_REQUEST['active']) and $_REQUEST['active'] == '99') {
 	$filter .= "  AND ( Active > '0') "; 
 }
+
 
 $page 		= $_REQUEST['page'];
 $length 	= $_REQUEST['length'];
@@ -55,15 +45,6 @@ $flds = array();
 $DB_Where = " " . $_REQUEST['where'];
 $DB_Where .= $filter;
 
-#********************************
-# kolone za koje je moguc Search 
-# treba ih samo nabrojati ovdje
-# Search ce ih sam pretraziti
-#********************************
-$aColumns = array(
-	'Title' // dodaj ostala polja!
-);
-
 # dodavanje search parametra u qry
 # DB_Where sad ima sve potrebno za qry
 if ( $_REQUEST['Search'] != "" )
@@ -82,38 +63,25 @@ if ( $_REQUEST['Search'] != "" )
 }
 
 
-
 $dbTotalRecords = $db->getKeysBy('ID ASC', '',$DB_Where);
-
 # test za LIMIT - trebalo bi ga iskoristiti za pagination! 'asc' . ' LIMIT 0,50'
 $dbk = $db->getKeysBy('DisplayOrder ' . $sortOrder, '' . $limit , $DB_Where);
 
 if (count($dbk) != 0) {
     foreach ($dbk as $nn => $key)
     {
-    	
     	$db->getRow($key);
-		
 		// ako treba neki lookup, onda to ovdje
-		
 		# get all fields and values
 		$detailFlds = $db->fieldValues();
-		
 		// ako postoji neko custom polje, onda to ovdje.
 		// npr. $detailFlds["AuthLevelName"] = $nekaDrugaDB->getAuthLevelName().' nesto';
-		
 		$out[] = $detailFlds;    	
-
-		
     }
 }
-
-
 # send output back
 $output = array(
 'recordsTotal' => count($dbTotalRecords),
 'data' =>$out
 );
-
-echo $_GET['callback'] . '(' . json_encode($output) . ')';
-	
+echo $_GET['callback'] . '(' . json_encode($output) . ')';	
