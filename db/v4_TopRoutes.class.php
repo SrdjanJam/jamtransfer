229bf77c18ce8b33d2b7589d2a71d356a222d056
MarkoJam <107544110+MarkoJam@users.noreply.github.com>
2023-03-08 11:59:04 +0100

    Create v4_TopRoutes.class.php

---
+++ db/v4_TopRoutes.class.php
@@ -0,0 +1,162 @@
+<?php
+/*
+ * Author: Rafael Rocha
+ *
+ * Changes: Bogo Soic-Mirilovic bogo.split@gmail.com
+ * 
+ * Version of MYSQL_to_PHP: 1.1.1
+ * 
+ * License: LGPL 
+ * 
+ */
+require_once 'db.class.php';
+
+Class v4_TopRoutes {
+
+	public $TopRouteID; //int(10) unsigned
+	public $Main; //int(1)
+	public $Description; //varchar 255
+	
+	public $connection;
+
+	function __construct(){
+		$this->connection = new DataBaseMysql();
+	}	public function myreal_escape_string($string){
+		return $this->connection->real_escape_string($string);
+	}
+
+    /**
+     * New object to the class. Don´t forget to save this new object "as new" by using the function $class->saveAsNew(); 
+     *
+     */
+	public function New_v4_TopRoutes($TopRouteID,$Main,$Description){
+		$this->TopRouteID = $TopRouteID; // Check
+		$this->Main = $Main;
+		$this->Description = $Description;
+	}
+
+    /**
+     * Load one row into var_class. To use the vars use for exemple echo $class->getVar_name; 
+     *
+     * @param key_table_type $key_row
+     * 
+     */
+	public function getRow($key_row){
+		$result = $this->connection->RunQuery("Select * from v4_TopRoutes where TopRouteID = \"$key_row\" ");
+		if($result->num_rows < 1) return false;
+		while($row = $result->fetch_array(MYSQLI_ASSOC)){
+			$this->TopRouteID = $row["TopRouteID"];
+			$this->Main = $row["Main"];
+			$this->Description = $row["Description"];
+		}
+	}
+
+    /**
+     * Delete the row by using the key as arg
+     *
+     * @param key_table_type $key_row
+     *
+     */
+	public function deleteRow($key_row){
+		$this->connection->RunQuery("DELETE FROM v4_TopRoutes WHERE TopRouteID = $key_row");
+	}
+
+    /**
+     * Update the active row table on table
+     */
+	public function saveRow(){
+		$result = $this->connection->RunQuery("UPDATE v4_TopRoutes set 
+		TopRouteID = '".$this->myreal_escape_string($this->TopRouteID)."', 
+		Main = '".$this->myreal_escape_string($this->Main)."', 
+		Description = '".$this->myreal_escape_string($this->Description)."' WHERE TopRouteID = '".$this->TopRouteID."'");
+		
+		return $result; 
+	}
+
+
+    /**
+     * Returns array of keys order by $column -> name of column $order -> desc or acs
+     *
+     * @param string $column
+     * @param string $order
+     */
+	public function getKeysBy($column, $order, $where = NULL){
+		$keys = array(); $i = 0;
+		$result = $this->connection->RunQuery("SELECT TopRouteID from v4_TopRoutes $where ORDER BY $column $order");
+			while($row = $result->fetch_array(MYSQLI_ASSOC)){
+				$keys[$i] = $row["TopRouteID"];
+				$i++;
+			}
+	return $keys;
+	}
+
+	/**
+	 * @return TopRouteID - int(10) unsigned
+	 */
+	public function getTopRouteID(){
+		return $this->TopRouteID;
+	}
+
+	/**
+	 * @return Main - int(1)
+	 */
+	public function getMain(){
+		return $this->Main;
+	}
+
+	/**
+	 * @return Description - varchar(255)
+	 */
+	public function getDescription(){
+		return $this->Description;
+	}
+	
+
+	public function setTopRouteID($TopRouteID){
+		$this->TopRouteID = $TopRouteID;
+	}
+
+	/**
+	 * @param Type: int(1)
+	 */
+	public function setMain($Main){
+		$this->Main = $Main;
+	}
+
+	/**
+	 * @param Type: varchar(255)
+	 */
+	public function setDescription($Description){
+		$this->Description = $Description;
+	}
+
+	
+
+
+	public function fieldValues(){
+		$fieldValues = array(
+			'TopRouteID' => $this->getTopRouteID(),
+			'Main' => $this->getMain(),
+			'Description' => $this->getDescription(),
+		);
+		return $fieldValues;
+	}
+    /**
+     * fieldNames - returns array of fieldNames 
+     *
+     * @param 
+     * 
+     */
+	public function fieldNames(){
+		$fieldNames = array(
+			'TopRouteID','Main', 'Description'	);
+		return $fieldNames;
+	}
+    /**
+     * Close mysql connection
+     */
+	public function endv4_Terminals(){
+		$this->connection->CloseMysql();
+	}
+
+}
