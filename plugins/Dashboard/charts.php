74fcf090cd5eccbc3997ad6361e7caefce104879
SrdjanJam <97429881+SrdjanJam@users.noreply.github.com>
2023-04-06 14:20:11 +0200

    'charts'

--- plugins/Dashboard/charts.php
+++ plugins/Dashboard/charts.php
@@ -15,11 +15,24 @@ $StatusDescription = array(
 */
 	require_once 'config.php';
 	require_once ROOT . '/db/v4_OrderDetails.class.php';
+	require_once ROOT . '/db/db.class.php';
+	
+	$db = new DataBaseMysql();
+	$q = "SELECT v4_AuthLevels.AuthLevelName as level,count(*) as value FROM `v4_OrderDetails`,v4_AuthLevels where `TransferStatus` <9 and  v4_AuthLevels.AuthLevelID=`UserLevelID` group by `UserLevelID`";
+	$r = $db->RunQuery($q);
+	$levels="[";
+	$values="[";
+	while ($t = $r->fetch_object()) {
+		$levels.="'".$t->level."'";
+		$levels.=",";		
+		$values.="'".$t->value."'";
+		$values.=",";		
+	}
+	$levels.="]";
+	$values.="]";
 
-    $od = new v4_OrderDetails();
-
-    $where = ' WHERE PickupDate >= "'.date("Y-m-d").'" AND TransferStatus < "3"';
-    $k = $od->getKeysBy('DetailsID', 'asc', $where);
+	$smarty->assign("levels",$levels);
+	$smarty->assign("values",$values);
 // filling data for charts
 
 $data1 = "data1";
--- plugins/Dashboard/templates/charts.tpl
+++ plugins/Dashboard/templates/charts.tpl
@@ -1,16 +1,14 @@
-<h1><i class="fa fa-bar-chart" aria-hidden="true"></i> Data presentation charts in progress</h1>
-
 <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.js"></script>
 
 <div style="padding:10px;">
-    <canvas id="myChart" style="width:100%;max-width:600px;"></canvas>
+    <canvas id="myChart" style="width:100%;max-width:700px;"></canvas>
 </div>
 
 
 {literal}
 <script>
-var xValues = [{/literal}"{$data1}"{literal},"France", "Spain", "USA", "Argentina"];
-var yValues = [{/literal} {$data2} {literal},49, 44, 24, 15];
+var xValues = {/literal}{$levels}{literal};
+var yValues = {/literal}{$values}{literal};
 var barColors = [
   "#b91d47",
   "#00aba9",
@@ -31,7 +29,7 @@ new Chart("myChart", {
   options: {
     title: {
       display: true,
-      text: "World Wide Wine Production 2018"
+      text: "Transfers purchaser"
     }
   }
 });
