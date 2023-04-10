cac8a94ab839dc368c1e26bb8ad705290880408f
SrdjanJam <97429881+SrdjanJam@users.noreply.github.com>
2023-04-10 14:28:43 +0200

    'paralel_tsls'

--- plugins/Tasks/ParalelTasks.php
+++ plugins/Tasks/ParalelTasks.php
@@ -1,26 +1,42 @@
 <?
 error_reporting(E_ALL);
-	$expense=109;
+	require_once ROOT . '/db/v4_SubVehicles.class.php';
+	require_once ROOT . '/db/v4_AuthUsers.class.php';
+	$sv = new v4_SubVehicles();
+	$sv->getRow($vehicleID);
+	$au = new v4_AuthUsers();
+	$smarty->assign('VehicleName',$sv->getVehicleDescription());
 
 	// check list
 	$checklist=array();
 	$ids="";
-
-
-	$sqls="SELECT `ID`,`Datum`  FROM `v4_SubActivity` WHERE `OwnerID`=".$_SESSION['UseDriverID']." and `Expense`=".$expense." and `VehicleID`=".$vehicleID." ORDER by Datum DESC Limit 5";
+	if ($expense<>'999') {
+		$whereAction="`ActionID`=".$expense;
+		$whereExpense="`Expense`=".$expense;
+	}	
+	else {
+		$whereAction="`ActionID`= 109 ";
+		$whereExpense="`Expense` in (109,127)";	
+	}	
+	
+	$sqls="SELECT `ID`,`Datum`,`Vreme`,`Expense`,`DriverID`,`Approved`  FROM `v4_SubActivity` WHERE `OwnerID`=".$_SESSION['UseDriverID']." and ".$whereExpense." and `VehicleID`=".$vehicleID." ORDER by Vreme DESC Limit 5";
 	$query=mysqli_query($db->conn, $sqls) or die('Error in SubActivity query' . mysqli_connect_error());
 	while($list = mysqli_fetch_object($query) ) {
 		$list_row=array();
 		$list_row['id']=$list->ID;
 		$list_row['datum']=$list->Datum;
+		$list_row['vreme']=$list->Vreme;
+		$list_row['expense']=$list->Expense;
+		$au->getRow($list->DriverID);
+		$list_row['drivername']=$au->getAuthUserRealName();
+		$list_row['approved']=$list->Approved;
 		$list_all[]=$list_row;
 		$ids.=$list->ID.",";
 	}	
 	$ids = substr($ids,0,strlen($ids)-1);
 	
-
 	$sqls2="SELECT * FROM `v4_ActionRequestItem`,`v4_Request` 
-		WHERE `ID`=v4_ActionRequestItem.`RequestID` AND `ActionID`=".$expense." ORDER BY `DisplayOrder`";
+		WHERE `ID`=v4_ActionRequestItem.`RequestID` AND ".$whereAction." ORDER BY `DisplayOrder`";
 	$query2=mysqli_query($db->conn, $sqls2) or die('Error in RequestCheckList query' . mysqli_connect_error());
 	while($list2 = mysqli_fetch_object($query2) ) {
 		$checklist_row=array();
--- plugins/Tasks/templates/paralelTask.tpl
+++ plugins/Tasks/templates/paralelTask.tpl
@@ -1,22 +1,32 @@
+	<h2>{$VehicleName}</h2>
 	<table>
 		<tr>
-			<th>
+			<th width='300px'>
 				Title
 			</th>
 			{section name=pom loop=$list_all}
-				<th>
-					{$list_all[pom].datum}
+				<th width='200px'>
+					{if $list_all[pom].expense eq '109'}Zaduženje{/if}
+					{if $list_all[pom].expense eq '127'}Razduženje{/if}
+					<br>
+					{$list_all[pom].vreme}
+					<br>
+					{$list_all[pom].drivername}		
+					<br>		
+					{if $list_all[pom].approved eq '1'}Approved{/if}
+					{if $list_all[pom].expense eq '0'}Not approved{/if}
+					
 				</th>		
 			{/section}	
 		</tr>
 		{section name=pom2 loop=$checklist}
 			<tr>
-				<td>
+				<td width='300px'>
 					{$checklist[pom2].title}
 				</td>
 				{section name=pom loop=$list_all}
-					<td>
-						<img width='100' src='{$checklist[pom2].photo[{$list_all[pom].id}]}'/>
+					<td width='200px'>
+						{if isset($checklist[pom2].photo[{$list_all[pom].id}])}<img width='100' src='{$checklist[pom2].photo[{$list_all[pom].id}]}'/>{/if}
 					</td>		
 				{/section}			
 			</tr>	
