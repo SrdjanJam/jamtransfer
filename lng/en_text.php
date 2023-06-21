fd4102b00e599a7018ea220d0229bad7a01219e0
MarkoJam <107544110+MarkoJam@users.noreply.github.com>
2023-06-21 12:15:25 +0200

    Update en_text.php

--- lng/en_text.php
+++ lng/en_text.php
@@ -34,111 +34,111 @@
 	);
 	*/
 
-	# TransferStatus - novi
-	$StatusDescription = array(
-		'1' =>    'Active',
-		'2' =>    'Changed',
-		'3' =>    'Canceled',
-		'4' =>    'TEMP',
-		'5'	=>	  'Completed',
-		'6'	=>	  'PreOrder'
-	);
+	// # TransferStatus - novi
+	// $StatusDescription = array(
+	// 	'1' =>    'Active',
+	// 	'2' =>    'Changed',
+	// 	'3' =>    'Canceled',
+	// 	'4' =>    'TEMP',
+	// 	'5'	=>	  'Completed',
+	// 	'6'	=>	  'PreOrder'
+	// );
 
-	# Change Transfer Reason Method
-	$ChangeTransferReason = array(
-		'1'	=>	'Pax FirstLast Name',
-		'2'	=>	'Pax Phone',
-		'3'	=>	'Pax Email',
-		'4'	=>	'Pickup Date',
-		'5'	=>	'Pickup Time',
-		'6'	=>	'Flight Time',
-		'7'	=>	'Flight Number',
-		'8'	=>	'Pickup Address',
-		'9'	=>	'Drop-Off Address',
-		'10'	=>	'Pax Number',
-		'11'	=>	'Payment method',
-		'12'	=>	'Partners Price',
-		'13'	=>	'Extras',
-		'14'	=>	'Pickup Notes',
-		'15'	=>	'Message'
-	);
+	// # Change Transfer Reason Method
+	// $ChangeTransferReason = array(
+	// 	'1'	=>	'Pax FirstLast Name',
+	// 	'2'	=>	'Pax Phone',
+	// 	'3'	=>	'Pax Email',
+	// 	'4'	=>	'Pickup Date',
+	// 	'5'	=>	'Pickup Time',
+	// 	'6'	=>	'Flight Time',
+	// 	'7'	=>	'Flight Number',
+	// 	'8'	=>	'Pickup Address',
+	// 	'9'	=>	'Drop-Off Address',
+	// 	'10'	=>	'Pax Number',
+	// 	'11'	=>	'Payment method',
+	// 	'12'	=>	'Partners Price',
+	// 	'13'	=>	'Extras',
+	// 	'14'	=>	'Pickup Notes',
+	// 	'15'	=>	'Message'
+	// );
 
 
-	# Payment Method
-	$PaymentMethod = array(
-		'1'	=>	'Online',
-		'2'	=>	'Cash',
-		'3'	=>	'Combined',
-		'4'	=>	'Bank transfer',
-		'5'	=>	'Compensation',
-		'6'	=>	'Bank transfer 2',
-		'9'	=>  'Other'
-	);
+	// # Payment Method
+	// $PaymentMethod = array(
+	// 	'1'	=>	'Online',
+	// 	'2'	=>	'Cash',
+	// 	'3'	=>	'Combined',
+	// 	'4'	=>	'Bank transfer',
+	// 	'5'	=>	'Compensation',
+	// 	'6'	=>	'Bank transfer 2',
+	// 	'9'	=>  'Other'
+	// );
 
-	# Payment Method
-	$AcceptedPayment = array(
-		'0'		=>	'Not selected',
-		'1'		=>	'All',
-		'2'		=>	'Online',
-		'3'		=>	'Cash',
-		'10'	=>	'Invoice',
-		'11'	=>	'Online',
-		'12'	=>	'Invoice 2',
-		'13'	=>	'Cash'
-	);
+	// # Payment Method
+	// $AcceptedPayment = array(
+	// 	'0'		=>	'Not selected',
+	// 	'1'		=>	'All',
+	// 	'2'		=>	'Online',
+	// 	'3'		=>	'Cash',
+	// 	'10'	=>	'Invoice',
+	// 	'11'	=>	'Online',
+	// 	'12'	=>	'Invoice 2',
+	// 	'13'	=>	'Cash'
+	// );
 
-	$PaymentStatus = array(
-		'0'	=>	'Not Paid',
-		'1'	=>	'Warning sent',
-		'2' =>	'Sued',
-		'3' =>  'Refunded',
-		'10'=>	'Lost - will not be paid',
-		'91'=>	'Compensated',
-		'99'=>	'Paid'
-	);
+	// $PaymentStatus = array(
+	// 	'0'	=>	'Not Paid',
+	// 	'1'	=>	'Warning sent',
+	// 	'2' =>	'Sued',
+	// 	'3' =>  'Refunded',
+	// 	'10'=>	'Lost - will not be paid',
+	// 	'91'=>	'Compensated',
+	// 	'99'=>	'Paid'
+	// );
 
-	# DriverConfStatus
-	$DriverConfStatus = array(
-		'0'	=> 'No Partner',
-		'1'	=> 'Not Confirmed',
-		'2'	=> 'Partner Confirmed',
-		'3' => 'Partner Assigned',
-		'4'	=> 'Partner Declined',
-		'5'	=> 'No-Show',
-		'6' => 'Partner Error',
-		'7' => 'Transfer Completed',
-		'8' => 'Operator Error',
-		'9' => 'Dispatcher Error',
-		'10' => 'Agent Error',
-	);
+	// # DriverConfStatus
+	// $DriverConfStatus = array(
+	// 	'0'	=> 'No Partner',
+	// 	'1'	=> 'Not Confirmed',
+	// 	'2'	=> 'Partner Confirmed',
+	// 	'3' => 'Partner Assigned',
+	// 	'4'	=> 'Partner Declined',
+	// 	'5'	=> 'No-Show',
+	// 	'6' => 'Partner Error',
+	// 	'7' => 'Transfer Completed',
+	// 	'8' => 'Operator Error',
+	// 	'9' => 'Dispatcher Error',
+	// 	'10' => 'Agent Error',
+	// );
 
-	# Driver Payment
-	$DriverPayment = array(
-		'0' => 'Not Paid',
-		'1' => 'Partly paid',
-		'2'	=> 'Paid',
-		'3' => 'Compensated'
-	);
-
-	
-	# Transfers Filters
-	$transfersFilters = array(
-		array ("id" => "noPartner", "name" => "No Partner"),
-		array ("id" => "notConfirmed", "name" => "Not Confirmed"),
-		array ("id" => "confirmed", "name" => "Confirmed"),
-		array ("id" => "declined", "name" => "Declined"),
-		array ("id" => "canceled", "name" => "Canceled"),
-		array ("id" => "noShow", "name" => "No Show"),
-		array ("id" => "PartnerError", "name" => "Partner Error"),
-		array ("id" => "agent", "name" => "Agent transfers"),
-		array ("id" => "notConfirmedAgent", "name" => "Agent transfers Not Confirmed"),
-		array ("id" => "notCompleted", "name" => "Not Completed"),
-		array ("id" => "invoice2", "name" => "Invoice 2"),
-	) ;
+	// # Driver Payment
+	// $DriverPayment = array(
+	// 	'0' => 'Not Paid',
+	// 	'1' => 'Partly paid',
+	// 	'2'	=> 'Paid',
+	// 	'3' => 'Compensated'
+	// );
 
 	
+	// # Transfers Filters ne koristi se u novom cms-u
+	// $transfersFilters = array(
+	// 	array ("id" => "noPartner", "name" => "No Partner"),
+	// 	array ("id" => "notConfirmed", "name" => "Not Confirmed"),
+	// 	array ("id" => "confirmed", "name" => "Confirmed"),
+	// 	array ("id" => "declined", "name" => "Declined"),
+	// 	array ("id" => "canceled", "name" => "Canceled"),
+	// 	array ("id" => "noShow", "name" => "No Show"),
+	// 	array ("id" => "PartnerError", "name" => "Partner Error"),
+	// 	array ("id" => "agent", "name" => "Agent transfers"),
+	// 	array ("id" => "notConfirmedAgent", "name" => "Agent transfers Not Confirmed"),
+	// 	array ("id" => "notCompleted", "name" => "Not Completed"),
+	// 	array ("id" => "invoice2", "name" => "Invoice 2"),
+	// ) ;
 
-	$dayNames = array("Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat");
+	
+
+	// $dayNames = array("Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat");
 
 
 
@@ -1193,3 +1193,5 @@ define("TO_DO_LIST","To Do List");
 define("DELETE_COMPLETED","Delete Completed");
 define("REPORT_BY","Report By");
 define("FILTER_BY","Filter By");
+
+define("PAYMENT_METHOD_STATUS","5");
