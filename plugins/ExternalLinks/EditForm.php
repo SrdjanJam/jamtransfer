658fefdcbe30360f67c63df9872f663a2bceca67
MarkoJam <107544110+MarkoJam@users.noreply.github.com>
2023-08-28 13:29:15 +0200

    Created External Links

---
+++ plugins/ExternalLinks/EditForm.php
@@ -0,0 +1,82 @@
+
+<script type="text/x-handlebars-template" id="ItemEditTemplate">
+<form id="ItemEditForm{{ID}}" class="form box box-info" enctype="multipart/form-data" method="post" onsubmit="return false;">
+
+	<div class="box-header">
+		<div class="box-tools pull-right">
+			<span id="statusMessage" class="text-info xl"></span>
+			
+			<? if (!$isNew) { ?>
+				<button class="btn btn-warning" title="<?= CLOSE?>" 
+				onclick="return editCloseItem('{{ID}}');">
+				<i class="fa fa-close"></i>
+				</button>
+
+				<button class="btn btn-danger" title="<?= CANCEL ?>" 
+				onclick="return deleteItem('{{ID}}');">
+				<i class="fa fa-ban"></i>
+				</button>
+			<? } ?>	
+
+			<button class="btn btn-info" title="<?= SAVE_CHANGES ?>" 
+			onclick="return editSaveItem('{{ID}}');">
+			<i class="fa fa-save"></i>
+			</button>
+		</div>
+	</div>
+
+	<div class="box-body ">
+        <div class="row">
+			<div class="col-md-6">
+				<!-- ID: -->
+				<div class="row">
+					<div class="col-md-2">
+						<label for="ID"><?=ID;?></label>
+					</div>
+					<div class="col-md-10">
+						<input type="text" name="ID" id="ID" class="w100" value="{{ID}}">
+					</div>
+				</div>
+				<!-- TITLE: -->
+				<div class="row">
+					<div class="col-md-2">
+						<label for="Title"><?=TITLE;?></label>
+					</div>
+					<div class="col-md-10">
+						<input type="text" name="Title" id="Title" class="w100" value="{{Title}}">
+					</div>
+				</div>
+				<!-- URL: -->
+				<div class="row">
+					<div class="col-md-2">
+						<label for="Url"><?=URL;?></label>
+					</div>
+					<div class="col-md-10">
+						<input type="text" name="Url" id="Url" class="w100" value="{{Url}}">
+					</div>
+				</div>
+				<!-- IMAGE: -->
+				<div class="row">
+					<div class="col-md-2">
+						<label for="Image"><?=IMAGE;?></label>
+					</div>
+					<div class="col-md-10">
+						<input type="Image" name="Image" id="Image" class="w100" value="{{Image}}">
+					</div>
+				</div>
+		
+			</div>	
+	    </div>
+	</div>	
+</form>
+
+
+	<script>
+		// uklanja ikonu Saved - statusMessage sa ekrana
+		$("form").change(function(){
+			$("#statusMessage").html('');
+		});
+	
+	</script>
+</script>
+	
---
+++ plugins/ExternalLinks/ListTemplate.php
@@ -0,0 +1,71 @@
+<script type="text/x-handlebars-template" id="ItemListTemplate">
+		<div id="ItemWrapperNew" class="editFrame" style="display:none">
+			<div id="inlineContentNew" class="row">
+				<div id="new_Item" >
+					<?= LOADING ?>
+				</div>
+			</div>
+		</div>
+
+	<!-- Labels: -->
+	<div class="row row-edit">
+		
+		<div class="col-md-2">
+			<?=ID;?>
+		</div>
+
+		<div class="col-md-2">
+			<?=TITLE?>
+		</div>
+
+		<div class="col-md-2">
+			<?=URL;?>
+		</div>		
+		
+		<div class="col-md-2">
+			<?=IMAGE;?>
+		</div>		
+				
+	</div>
+	<!-- --------------------------------- -->
+
+	{{#each Item}}
+		<div  onclick="oneItem({{ID}});">
+		
+			<div class="row {{color}} pad1em listTile" 
+			style="border-top:1px solid #ddd" 
+			id="t_{{ID}}">
+		
+					<div class="col-md-2">
+						<strong>{{ID}}</strong>
+					</div>
+
+					<div class="col-md-2">
+						{{Title}}
+					</div>
+
+					<div class="col-md-2">
+						{{Url}}
+					</div>			
+					
+					<div class="col-md-2">
+						{{Image}}
+					</div>
+					
+			</div>
+		</div>
+		<div id="ItemWrapper{{ID}}" class="editFrame" style="display:none">
+			<div id="inlineContent{{ID}}" class="row">
+				<div id="one_Item{{ID}}" >
+					<?= LOADING ?>
+				</div>
+			</div>
+		</div>
+
+	{{/each}}
+
+
+
+</script>
+	
+
