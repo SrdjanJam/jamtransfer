0042fc3ff27b062a66d137a93149a6244e9542e2
MarkoJam <107544110+MarkoJam@users.noreply.github.com>
2024-01-10 14:04:40 +0100

    Update Delete.php

--- plugins/Request/Delete.php
+++ plugins/Request/Delete.php
@@ -5,7 +5,10 @@ require_once 'Initial.php';
 $out = array();
 
 # delete row by key value
-$db->deleteRow($_REQUEST['ID']);
+// if (isset($_SESSION['UseDriverID']) && $_SESSION['UseDriverID']>0) 
+// 	$result = $dbT->RunQuery("DELETE FROM `v4_Request` WHERE `ID`=".$_REQUEST['ID']." AND `OwnerID`=".$_SESSION['UseDriverID']);	
+// else $db->deleteRow($_REQUEST['ID']);
+$db->deleteRow($_REQUEST['id']);
 $out[] = 'Deleted';
 
 # send output back
