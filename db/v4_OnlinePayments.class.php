928cb01ed658c06f0083947573ee8bd4e19c74f4
MarkoJam <jam.bgprogrameri4@gmail.com>
2024-02-05 14:10:22 +0100

    Update v4_OnlinePayments.class.php

--- db/v4_OnlinePayments.class.php
+++ db/v4_OnlinePayments.class.php
@@ -15,6 +15,17 @@ Class v4_OnlinePayments {
 	public $datetime3; //datetime
 	public $created_at; //timestamp
 	public $updated_at; //timestamp
+// New fields:
+	public $MonriID; //int(11)
+	public $Buyer; //varchar(255)
+	public $Country; //varchar(50)
+	public $Card; //varchar(20)
+	public $Amount; //decimal(10,2)
+	public $Currency; //varchar(3)
+	public $Avans; //tinyint(1)
+	public $EU; //tinyint(1)
+	public $PickupDate; //date
+	public $FiscalBill; //varchar(20)
 
 
 	public $connection;
@@ -31,7 +42,7 @@ Class v4_OnlinePayments {
      * New object to the class. Don´t forget to save this new object "as new" by using the function $class->saveAsNew(); 
      *
      */
-	public function New_v4_OnlinePayments($ID,$gateway,$OrderID,$CustomerIP,$OrderNumber,$type,$datetime1,$datetime2,$datetime3,$created_at,$updated_at){
+	public function New_v4_OnlinePayments($ID,$gateway,$OrderID,$CustomerIP,$OrderNumber,$type,$datetime1,$datetime2,$datetime3,$created_at,$updated_at,$MonriID,$Buyer,$Country,$Card,$Amount,$Currency,$Avans,$EU,$PickupDate,$FiscalBill){
 		$this->ID = $ID;
 		$this->gateway = $gateway;
 		$this->OrderID = $OrderID;		
@@ -43,6 +54,16 @@ Class v4_OnlinePayments {
 		$this->datetime3 = $datetime3;
 		$this->created_at = $created_at;
 		$this->updated_at = $updated_at;
+		$this->MonriID = $MonriID;
+		$this->Buyer = $Buyer;
+		$this->Country = $Country;
+		$this->Card = $Card;
+		$this->Amount = $Amount;
+		$this->Currency = $Currency;
+		$this->Avans = $Avans;
+		$this->EU = $EU;
+		$this->PickupDate = $PickupDate;
+		$this->FiscalBill = $FiscalBill;
 	}
 
     /**
@@ -66,6 +87,16 @@ Class v4_OnlinePayments {
 			$this->datetime3 = $row["datetime3"];
 			$this->created_at = $row["created_at"];
 			$this->updated_at = $row["updated_at"];
+			$this->MonriID = $row["MonriID"];
+			$this->Buyer = $row["Buyer"];
+			$this->Country = $row["Country"];
+			$this->Card = $row["Card"];
+			$this->Amount = $row["Amount"];
+			$this->Currency = $row["Currency"];
+			$this->Avans = $row["Avans"];
+			$this->EU = $row["EU"];
+			$this->PickupDate = $row["PickupDate"];
+			$this->FiscalBill = $row["FiscalBill"];
 		}
 	}
 
@@ -99,14 +130,51 @@ Class v4_OnlinePayments {
 		$result = $this->connection->RunQuery("UPDATE v4_OnlinePayments set 
 gateway = '".$this->myreal_escape_string($this->gateway)."', 
 OrderID = '".$this->myreal_escape_string($this->OrderID)."', 
+CustomerIP = '".$this->myreal_escape_string($this->CustomerIP)."', 
 OrderNumber = '".$this->myreal_escape_string($this->OrderNumber)."', 
 type = '".$this->myreal_escape_string($this->type)."', 
 datetime1 = '".$this->myreal_escape_string($this->datetime1)."', 
 datetime2 = '".$this->myreal_escape_string($this->datetime2)."', 
 datetime3 = '".$this->myreal_escape_string($this->datetime3)."', 
 created_at = '".$this->myreal_escape_string($this->created_at)."', 
-updated_at = '".$this->myreal_escape_string($this->updated_at)."' WHERE ID = '".$this->ID."'");
+updated_at = '".$this->myreal_escape_string($this->updated_at)."',
+MonriID = '".$this->myreal_escape_string($this->MonriID)."',
+Buyer = '".$this->myreal_escape_string($this->Buyer)."',
+Country = '".$this->myreal_escape_string($this->Country)."',
+Card = '".$this->myreal_escape_string($this->Card)."',
+Amount = '".$this->myreal_escape_string($this->Amount)."',
+Currency = '".$this->myreal_escape_string($this->Currency)."',
+Avans = '".$this->myreal_escape_string($this->Avans)."',
+EU = '".$this->myreal_escape_string($this->EU)."',
+PickupDate = '".$this->myreal_escape_string($this->PickupDate)."',
+FiscalBill = '".$this->myreal_escape_string($this->FiscalBill)."' WHERE ID = '".$this->ID."'");
 	return $result; 
+}
+
+
+public function saveAsNew(){
+	$this->connection->RunQuery("INSERT INTO v4_OnlinePayments (gateway, OrderID, CustomerIP, OrderNumber, type, datetime1, datetime2, datetime3, created_at, updated_at,MonriID, Buyer, Country, Card, Amount, Currency, Avans, EU, PickupDate, FiscalBill) values (
+		'".$this->myreal_escape_string($this->gateway)."', 
+		'".$this->myreal_escape_string($this->OrderID)."', 
+		'".$this->myreal_escape_string($this->CustomerIP)."', 
+		'".$this->myreal_escape_string($this->OrderNumber)."', 
+		'".$this->myreal_escape_string($this->type)."', 
+		'".$this->myreal_escape_string($this->datetime1)."', 
+		'".$this->myreal_escape_string($this->datetime2)."', 
+		'".$this->myreal_escape_string($this->datetime3)."',
+		'".$this->myreal_escape_string($this->created_at)."',
+		'".$this->myreal_escape_string($this->updated_at)."',
+		'".$this->myreal_escape_string($this->MonriID)."',
+		'".$this->myreal_escape_string($this->Buyer)."',
+		'".$this->myreal_escape_string($this->Country)."',
+		'".$this->myreal_escape_string($this->Card)."',
+		'".$this->myreal_escape_string($this->Amount)."',
+		'".$this->myreal_escape_string($this->Currency)."',
+		'".$this->myreal_escape_string($this->Avans)."',
+		'".$this->myreal_escape_string($this->EU)."',
+		'".$this->myreal_escape_string($this->PickupDate)."',
+		'".$this->myreal_escape_string($this->FiscalBill)."')");
+	return $this->connection->insert_id(); //return insert_id
 }
 
 	// GET METHODS:
@@ -187,6 +255,77 @@ updated_at = '".$this->myreal_escape_string($this->updated_at)."' WHERE ID = '".
 	public function getupdated_at(){
 		return $this->updated_at;
 	}
+
+	/**
+	 * @return MonriID - //int(11)
+	 */
+	public function getMonriID(){
+		return $this->MonriID;
+	}
+
+	/**
+	 * @return Buyer - //varchar(255)
+	 */
+	public function getBuyer(){
+		return $this->Buyer;
+	}
+
+	/**
+	 * @return Country - //varchar(50)
+	 */
+	public function getCountry(){
+		return $this->Country;
+	}
+
+	/**
+	 * @return Card - //varchar(20)
+	 */
+	public function getCard(){
+		return $this->Card;
+	}
+
+	/**
+	 * @return Amount - //decimal(10,2)
+	 */
+	public function getAmount(){
+		return $this->Amount;
+	}
+
+	/**
+	 * @return Currency - //varchar(3)
+	 */
+	public function getCurrency(){
+		return $this->Currency;
+	}
+
+	/**
+	 * @return Avans - //tinyint(1)
+	 */
+	public function getAvans(){
+		return $this->Avans;
+	}
+
+	/**
+	 * @return EU - //tinyint(1)
+	 */
+	public function getEU(){
+		return $this->EU;
+	}
+
+	/**
+	 * @return PickupDate - //date
+	 */
+	public function getPickupDate(){
+		return $this->PickupDate;
+	}
+
+	/**
+	 * @return FiscalBill - //varchar(20)
+	 */
+	public function getFiscalBill(){
+		return $this->FiscalBill;
+	}
+
 
 
 
@@ -269,6 +408,76 @@ updated_at = '".$this->myreal_escape_string($this->updated_at)."' WHERE ID = '".
 	public function setupdated_at($updated_at){
 		$this->updated_at = $updated_at;
 	}
+
+	/**
+	 * @param Type: //int(11)
+	 */
+	public function setMonriID($MonriID){
+		$this->MonriID = $MonriID;
+	}
+
+	/**
+	 * @param Type: //varchar(255)
+	 */
+	public function setBuyer($Buyer){
+		$this->Buyer = $Buyer;
+	}
+
+	/**
+	 * @param Type: //varchar(50)
+	 */
+	public function setCountry($Country){
+		$this->Country = $Country;
+	}
+
+	/**
+	 * @param Type: //varchar(20)
+	 */
+	public function setCard($Card){
+		$this->Card = $Card;
+	}
+
+	/**
+	 * @param Type: //decimal(10,2)
+	 */
+	public function setAmount($Amount){
+		$this->Amount = $Amount;
+	}
+
+	/**
+	 * @param Type: //varchar(3)
+	 */
+	public function setCurrency($Currency){
+		$this->Currency = $Currency;
+	}
+
+	/**
+	 * @param Type: //tinyint(1)
+	 */
+	public function setAvans($Avans){
+		$this->Avans = $Avans;
+	}
+
+	/**
+	 * @param Type: //tinyint(1)
+	 */
+	public function setEU($EU){
+		$this->EU = $EU;
+	}
+
+	/**
+	 * @param Type: //date
+	 */
+	public function setPickupDate($PickupDate){
+		$this->PickupDate = $PickupDate;
+	}
+
+	/**
+	 * @param Type: //varchar(20)
+	 */
+	public function setFiscalBill($FiscalBill){
+		$this->FiscalBill = $FiscalBill;
+	}
 	
 	// ===========================================================================================
 
@@ -291,7 +500,17 @@ updated_at = '".$this->myreal_escape_string($this->updated_at)."' WHERE ID = '".
 			'datetime2' => $this->getdatetime2(),
 			'datetime3' => $this->getdatetime3(),
 			'created_at' => $this->getcreated_at(),
-			'updated_at' => $this->getupdated_at()
+			'updated_at' => $this->getupdated_at(),
+			'MonriID' => $this->getMonriID(),
+			'Buyer' => $this->getBuyer(),
+			'Country' => $this->getCountry(),			
+			'Card' => $this->getCard(),			
+			'Amount' => $this->getAmount(),		
+			'Currency' => $this->getCurrency(),		
+			'Avans' => $this->getAvans(),	
+			'EU' => $this->getEU(),
+			'PickupDate' => $this->getPickupDate(),
+			'FiscalBill' => $this->getFiscalBill()
 		);
 		return $fieldValues;
 	}
@@ -314,7 +533,17 @@ updated_at = '".$this->myreal_escape_string($this->updated_at)."' WHERE ID = '".
 			'datetime2',
 			'datetime3',
 			'created_at',
-			'updated_at'
+			'updated_at',
+			'MonriID',
+			'Buyer',
+			'Country',
+			'Card',
+			'Amount',
+			'Currency',
+			'Avans',
+			'EU',
+			'PickupDate',
+			'FiscalBill'
 
 		);
 		return $fieldNames;
