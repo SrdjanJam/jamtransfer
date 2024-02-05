6723fe81750c4f2da1700c8297f526e09b556ae6
MarkoJam <jam.bgprogrameri4@gmail.com>
2024-02-05 10:48:53 +0100

    Update Initial.php

--- plugins/SubVehicles/Initial.php
+++ plugins/SubVehicles/Initial.php
@@ -1,7 +1,9 @@
 <?
 require_once '../../config.php';
 require_once ROOT . '/db/v4_SubVehicles.class.php';
+require_once ROOT . '/db/v4_VehicleTypes.class.php';
 $db = new v4_SubVehicles();
+$vt = new v4_VehicleTypes();
 $dbT = new DataBaseMysql();
 
 $keyName = 'VehicleID';
